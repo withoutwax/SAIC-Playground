2af35d817170981500a0bb11a2f6282a21ec1f6a
Will Kim <rlagmlckd@gmail.com>
2018-11-15 16:17:14 -0600

    Basic Setup for App

---
+++ src/Canvas.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Canvas extends Component {
+  render() {
+    return (
+      <div className="Canvas">
+        Canvas
+      </div>
+    )
+  }
+}
+
+export default Canvas;
---
+++ src/Menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Menu">
+        Menu
+      </div>
+    )
+  }
+}
+
+export default Menu;
